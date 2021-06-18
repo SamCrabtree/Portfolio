16188c00d3b23ae4923c742dca178115e24df776
Sam Crabtree <samuel.crabtree@protonmail.com>
2021-06-17 22:41:45 -0400

    Finished base page layout

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log('Hello World!')
+console.log('Hello World!') //This is in place to ensure the script is connected properly.
+
+
+
+function toggleMenu() {
+    let target = document.getElementById("menu_items");
+  
+    if (target.style.display === "block") {
+      target.style.display = "none";
+    } else {
+      target.style.display = "block";
+    }
+  }
