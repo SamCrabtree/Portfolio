3f67d43077f30e7a38154b4fb3835499241a36d8
Sam Crabtree <samuel.crabtree@protonmail.com>
2021-06-19 21:06:47 -0400

    Adds simplified stylesheet and script

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    let target = document.getElementById("menu");
+  
+    if (target.style.display === "flex") {
+      target.style.display = "none";
+    } else {
+      target.style.display = "flex";
+    }
+  }
